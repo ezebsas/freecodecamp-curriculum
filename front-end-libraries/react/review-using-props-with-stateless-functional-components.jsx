a12c707104d30933e5a27988b0bd3becd006ef2d
eze <mollinedoezequiel@gmail.com>
2019-10-11 10:40:01 -0300

    react till stateless components

---
+++ front-end-libraries/react/review-using-props-with-stateless-functional-components.jsx
@@ -0,0 +1,30 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+
+class Camper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+};
+Camper.propTypes = { name: PropTypes.string.isRequired }
+
+Camper.defaultProps = {
+  name: 'CamperBot'
+};
---
+++ front-end-libraries/react/use-default-props.jsx
@@ -0,0 +1,8 @@
+const ShoppingCart = (props) => {
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+ShoppingCart.defaultProps = { items: 0 }
