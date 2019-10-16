14acd9e6b319f648670e9a5cdd855b6537feb9bf
eze <mollinedoezequiel@gmail.com>
2019-10-16 15:42:16 -0300

    finish redux part

---
+++ front-end-libraries/redux/copy-an-object-with-object.assign.js
@@ -0,0 +1,27 @@
+// The Redux state and actions were modified to handle an object for the state. Edit the code to return a new state object for actions with type ONLINE, which set the status property to the string online. Try to use Object.assign() to complete the challenge.
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      const newState = Object.assign({}, state);
+      newState.status = 'online'
+      return newState;
+      return
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
